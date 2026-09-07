bf71bce45149e489c0c35c54fcd50dc68214c2e6
Nur Munoz <n.munoz21@ufromail.cl>
2026-09-07 19:28:23 -0300

    Se termino de desarrollar la declaracion de login

--- src/test/java/VentanaLogin.java
+++ src/test/java/VentanaLogin.java
@@ -62,7 +62,20 @@ public class VentanaLogin {
      * ventana o mostrar un mensaje de error.
      */
     private void login() {
-// TODO: Implementar la lógica de inicio de sesión
+        String u= txtUsuario.getText();
+        String p= new String(txtClave.getPassword());
+
+        String nombreUsuario= validarCredenciales(u,p);
+
+        if (!nombreUsuario.isEmpty()){
+            JOptionPane.showMessageDialog(frame,"BIENVENIDO AL CASINO " + nombreUsuario);
+            frame.dispose();
+            // Conecta a la ruleta en consola
+            Ruleta.main(new String[]{});
+        }else{
+            JOptionPane.showMessageDialog(frame,"USUARIO O CONTRASEÑA INCORRECTA");
+        }
+
     }
     /**
      * Valida las credenciales ingresadas utilizando la lista de usuarios.
